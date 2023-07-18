d84868f1a98816bf437249a0891da1ca31b41a27
Sani <pilyugaa4@gmail.com>
2023-07-19 00:36:47 +0300

    feat: browser worker setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import {browserWorker} from './mocks/browser.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+declare global {
+    interface Window {
+        __mswStop: () => void;
+    }
+}
+
+const prepareMockServiceWorker = async () => {
+    if (import.meta.env.MODE === 'development') {
+        //* after worker activation u will get console.log: [MSW] Mocking enabled
+        //* waitUntilReady - default true (don't intercept requests until initialization gets ready)
+        await browserWorker.start({waitUntilReady: true});
+
+        //* Write the stop method on the window
+        //* to access during runtime.
+        window.__mswStop = browserWorker.stop;
+    }
+};
+
+prepareMockServiceWorker().then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+});
---
+++ src/mocks/browser.ts
@@ -0,0 +1,9 @@
+// src/mocks/browser.js
+import {setupWorker} from 'msw';
+import {handlers} from './handlers';
+
+// This configures a Service Worker with the given request handlers.
+export const browserWorker = setupWorker(...handlers);
+
+//!  self.registration.unregister() - remove it if create-react-app <= 3 version (public\mockServiceWorker.js)
+//* in your src/index.js file. Create React App unregisters all Service Workers by default, which would also unregister the mock Service Worker, resulting into broken requests interception.
